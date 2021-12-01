00e19606d1adcfe4dc65ea7f082795ad8bcf341e
ikjo93 <82401504+ikjo93@users.noreply.github.com>
2021-12-02 01:53:09 +0900

    코드업-3704(2021-12-01)

--- Code-up/계단오르기2/Main.java
+++ Code-up/계단오르기2/Main.java
@@ -9,13 +9,9 @@ class Main {
         List<Integer> list = new ArrayList<>();
 
         for (int i = 1; i <= n; i++) {
-            if(i<=2) {
-                list.add(i);
-            } else if(i==3) {
-                list.add(4);
-            } else {
-                list.add((list.get(i-2)+list.get(i-3)+list.get(i-4))%1000);
-            }
+            if(i<=2) list.add(i);
+            else if(i==3) list.add(4);
+            else list.add((list.get(i-2)+list.get(i-3)+list.get(i-4))%1000);
         }
 
         System.out.println(list.get(n-1));
