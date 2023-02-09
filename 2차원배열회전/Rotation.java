70d5d367892a1c6a5fc0560bddbb24140174595a
ikjo93 <82401504+ikjo93@users.noreply.github.com>
2023-02-09 12:10:19 +0900

    Create Rotation.java

---
+++ 2차원배열회전/Rotation.java
@@ -0,0 +1,15 @@
+class Rotation {
+
+    public int[][] rotateClockwise(int[][] arr) {
+        int row = arr.length;
+        int col = arr[0].length;
+        int[][] newKey = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                newKey[j][col - 1 - i] = arr[i][j];
+            }
+        }
+
+        return newKey;
+    }
+}
