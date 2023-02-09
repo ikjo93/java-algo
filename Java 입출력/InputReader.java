1e13565e3279a50f9e4e588bbaafbbc1dd3319fe
ikjo93 <82401504+ikjo93@users.noreply.github.com>
2023-02-09 14:35:00 +0900

    Create InputReader.java

---
+++ Java 입출력/InputReader.java
@@ -0,0 +1,49 @@
+class InputReader {
+
+    private final InputStream stream;
+    private final byte[] buf = new byte[8192];
+    private int curChar, snumChars;
+
+    public InputReader(InputStream st) {
+        this.stream = st;
+    }
+
+    public int read() {
+        if (snumChars == -1)
+            throw new InputMismatchException();
+        if (curChar >= snumChars) {
+            curChar = 0;
+            try {
+                snumChars = stream.read(buf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (snumChars <= 0)
+                return -1;
+        }
+        return buf[curChar++];
+    }
+
+    public int nextInt() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        int res = 0;
+        do {
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    public boolean isSpaceChar(int c) {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+}
