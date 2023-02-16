5a9d50f1dc7ba2074d85ae345ed5f896f298fd4d
ikjo93 <82401504+ikjo93@users.noreply.github.com>
2023-02-17 00:34:41 +0900

    Update Main_12865_평벙함 배낭.java

--- Algorithms/다이나믹프로그래밍/BOTTOM-UP/BOJ/Main_12865_평벙함 배낭.java
+++ Algorithms/다이나믹프로그래밍/BOTTOM-UP/BOJ/Main_12865_평벙함 배낭.java
@@ -5,40 +5,27 @@ import java.util.StringTokenizer;
 
 class Main {
 
-    static int[] w, v;
-    static Integer[][] bag;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
-        w = new int[n];
-        v = new int[n];
+        int[] w = new int[n + 1];
+        int[]  v = new int[n + 1];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             w[i] = Integer.parseInt(st.nextToken());
             v[i] = Integer.parseInt(st.nextToken());
         }
 
-        bag = new Integer[n][k + 1];
+        int[] bag = new int[k + 1];
 
-        System.out.println(pack(n - 1, k));
-    }
+        for (int i = 1; i <= n; i++) {
+            for (int j = k; j - w[i] > -1; j--) {
+                bag[j] = Math.max(bag[j], bag[j - w[i]] + v[i]);
+            }
+        }
 
-    private static int pack(int i, int k) {
-       if (i < 0) {
-           return 0;
-       }
-
-       if (bag[i][k] == null) {
-           if (w[i] > k) {
-               bag[i][k] = pack(i - 1, k);
-           } else {
-               bag[i][k] = Math.max(pack(i - 1, k), pack(i - 1, k - w[i]) + v[i]);
-           }
-       }
-
-       return bag[i][k];
+        System.out.println(bag[k]);
     }
 }
