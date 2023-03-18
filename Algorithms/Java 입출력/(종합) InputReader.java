412bacd9e58e968527e6f95e8da3b77f5eee94e5
ikjo93 <82401504+ikjo93@users.noreply.github.com>
2023-03-18 22:31:12 +0900

    Update (종합) InputReader.java

--- Algorithms/Java 입출력/(종합) InputReader.java
+++ Algorithms/Java 입출력/(종합) InputReader.java
@@ -2,107 +2,102 @@ import java.io.*;
 
 class InputReader {
 
-	private final InputStream stream;
-	private final byte[] buf = new byte[8192];
-	private int curChar, snumChars;
+    private final InputStream stream = System.in;
+    private final byte[] buf = new byte[8192];
+    private int curChar, sumChars;
 
-	public InputReader(InputStream st) {
-		this.stream = st;
-	}
+    public int nextInt() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        int res = 0;
+        do {
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
 
-	public int read() {
-		if (snumChars == -1)
-			throw new InputMismatchException();
-		if (curChar >= snumChars) {
-			curChar = 0;
-			try {
-				snumChars = stream.read(buf);
-			} catch (IOException e) {
-				throw new InputMismatchException();
-			}
-			if (snumChars <= 0)
-				return -1;
-		}
-		return buf[curChar++];
-	}
+    public long nextLong() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        long res = 0;
+        do {
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
 
-	public int nextInt() {
-		int c = read();
-		while (isSpaceChar(c)) {
-			c = read();
-		}
-		int sgn = 1;
-		if (c == '-') {
-			sgn = -1;
-			c = read();
-		}
-		int res = 0;
-		do {
-			res *= 10;
-			res += c - '0';
-			c = read();
-		} while (!isSpaceChar(c));
-		return res * sgn;
-	}
+    public int[] nextIntArray(int n) {
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = nextInt();
+        }
+        return a;
+    }
 
-	public long nextLong() {
-		int c = read();
-		while (isSpaceChar(c)) {
-			c = read();
-		}
-		int sgn = 1;
-		if (c == '-') {
-			sgn = -1;
-			c = read();
-		}
-		long res = 0;
-		do {
-			res *= 10;
-			res += c - '0';
-			c = read();
-		} while (!isSpaceChar(c));
-		return res * sgn;
-	}
+    public String readString() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isSpaceChar(c));
+        return res.toString();
+    }
 
-	public int[] nextIntArray(int n) {
-		int a[] = new int[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = nextInt();
-		}
-		return a;
-	}
+    public String nextLine() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isEndOfLine(c));
+        return res.toString();
+    }
 
-	public String readString() {
-		int c = read();
-		while (isSpaceChar(c)) {
-			c = read();
-		}
-		StringBuilder res = new StringBuilder();
-		do {
-			res.appendCodePoint(c);
-			c = read();
-		} while (!isSpaceChar(c));
-		return res.toString();
-	}
+    private int read() {
+        if (sumChars == -1)
+            throw new InputMismatchException();
+        if (curChar >= sumChars) {
+            curChar = 0;
+            try {
+                sumChars = stream.read(buf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (sumChars <= 0)
+                return -1;
+        }
+        return buf[curChar++];
+    }
+    
+    private boolean isSpaceChar(int c) {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
 
-	public String nextLine() {
-		int c = read();
-		while (isSpaceChar(c))
-			c = read();
-		StringBuilder res = new StringBuilder();
-		do {
-			res.appendCodePoint(c);
-			c = read();
-		} while (!isEndOfLine(c));
-		return res.toString();
-	}
-
-	public boolean isSpaceChar(int c) {
-		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-	}
-
-	private boolean isEndOfLine(int c) {
-		return c == '\n' || c == '\r' || c == -1;
-	}
-
+    private boolean isEndOfLine(int c) {
+        return c == '\n' || c == '\r' || c == -1;
+    }
 }
